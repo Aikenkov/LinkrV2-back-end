65445d19a999be9b3f49dff5f2e668afa4c641cf
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-10-18 20:47:47 -0300

    init

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+server.get("/status", (req, res) => {
+    res.send("Ok status");
+});
+
+server.listen(process.env.PORT, () => {
+    console.log(`Listening on port: ${process.env.PORT}`);
+});
