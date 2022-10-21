423bb4188bfde538d54fdbda4178b2dc52cecac2
Enzo <enzo202p@gmail.com>
2022-10-20 21:58:54 -0300

    fix: router conflict

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -3,10 +3,6 @@ import { authRouter } from './authRoutes.js';
 import { hashtagsRouter } from './hashtagsRouter.js';
 import { timelineRouter } from './timelineRouter.js';
 import { userRouter } from './userRoutes.js';
-import { Router } from "express";
-import { authRouter } from "./authRoutes.js";
-import { timelineRouter } from "./timelineRouter.js";
-import { userRouter } from "./userRoutes.js";
 import { postRouter } from "./postRouter.js";
 
 const router = Router();
