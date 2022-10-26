060a42797e59a6c6baae2277830d3a0f08f3d2db
Gustavo <gumrqss@gmail.com>
2022-10-26 11:23:08 -0300

    search users followed

--- src/controllers/searchUsersControllers.js
+++ src/controllers/searchUsersControllers.js
@@ -1,4 +1,5 @@
 
+import connection from "../database/database.js";
 import { STATUS_CODE } from "../enums/statusCode.js";
 import { getsearchUsers } from "../repositories/searchUsersRepository.js";
 
@@ -16,6 +17,23 @@ export async function getUsers(req,res){
         return res.status(STATUS_CODE.NOT_FOUND).send('usuário não encontrado')
     }
 
+    } catch (error) {
+        return res.sendStatus(STATUS_CODE.SERVER_ERROR)
+    }
+}
+
+export async function getUsersFollow(req,res){
+
+    try {
+        const user = res.locals.user
+        const idFollowed = await connection.query(`
+            SELECT followed FROM
+                follows
+            WHERE
+                follows.follower = ($1)
+        `,[user])
+        return res.status(STATUS_CODE.OK).send(idFollowed.rows)
+        
     } catch (error) {
         return res.sendStatus(STATUS_CODE.SERVER_ERROR)
     }
--- src/routes/searchUsersRoutes.js
+++ src/routes/searchUsersRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getUsers } from '../controllers/searchUsersControllers.js';
+import { getUsers, getUsersFollow } from '../controllers/searchUsersControllers.js';
+import { validateToken } from '../middlewares/authMiddleware.js';
 
 
 
 const searchUsers = Router();
 
 searchUsers.get("/search", getUsers);
-
+searchUsers.get("/followed", validateToken, getUsersFollow )
 export{ searchUsers }
