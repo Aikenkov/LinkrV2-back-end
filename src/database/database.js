901b55ff8c12b4d14ecaf0c4f36f79902ee2c3d2
Enzo <enzo202p@gmail.com>
2022-10-28 16:07:23 -0300

    fix: prepare for deploy

--- src/database/database.js
+++ src/database/database.js
@@ -9,6 +9,9 @@ dotenv.config();
 
 const databaseConfig = {
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const connection = new Pool(databaseConfig);
